72a203973b6c8d1f8accfb784ade908ea093fec5
SK627@myscc.ca <SK627@myscc.ca>
2022-11-05 19:36:47 -0400

    Assignment 1

---
+++ assignments/src/Assignment1.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+export default function Assignment1(){
+    const [temp, setTemp] = useState(15)
+    return(
+    
+        <div>
+            <h1>{temp}</h1>
+        </div>
+    )
+}
--- assignments/src/index.js
+++ assignments/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './App';
 import reportWebVitals from './reportWebVitals';
+import Assignment1 from './Assignment1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<Assignment1 />
   </React.StrictMode>
 );
 
