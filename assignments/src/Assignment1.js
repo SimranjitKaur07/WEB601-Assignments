c96f09adf29d09cc34062b4a5441baec8617bb2c
SK627@myscc.ca <SK627@myscc.ca>
2022-11-06 18:01:37 -0500

    set as wuto cold hot button

--- assignments/src/Assignment1.js
+++ assignments/src/Assignment1.js
@@ -2,12 +2,23 @@ import React, {useState} from "react";
 
 export default function Assignment1(){
     const [temp, setTemp] = useState(15)
+    const [mode, setMode] = useState("Hot")
     return(
     
         <div>
             <h1>{temp}</h1>
             <button onClick={()=>{setTemp(temp+1)}}>+</button>
             <button onClick={()=>{setTemp(temp-1)}}>-</button>
+
+            <h2>{mode}</h2>
+            <hr />
+            <br />
+
+            <button onClick={()=>{setMode("Auto")}}>Auto</button>
+            <button onClick={()=>{setMode("Hot")}}>Hot</button>
+            <button onClick={()=>{setMode("Cold")}}>Cold</button>
+
+
         </div>
     )
 }
