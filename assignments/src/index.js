f1e3540ed7d371daeb3591a941c0ab61054d104d
SK627@myscc.ca <SK627@myscc.ca>
2022-11-06 23:46:44 -0500

    Update index.js

--- assignments/src/index.js
+++ assignments/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './App';
-import App from './App';
+
+
+import Movies from './Movies';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<App />
+
+<Movies />
   </React.StrictMode>
 );
 
