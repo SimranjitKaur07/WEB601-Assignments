d75a1cd561f73654ff1e6c9a20bddf30d0363b33
SK627@myscc.ca <SK627@myscc.ca>
2022-11-06 22:06:05 -0500

    Done

--- assignments/src/Assignment1.js
+++ assignments/src/Assignment1.js
@@ -3,20 +3,37 @@ import React, {useState} from "react";
 export default function Assignment1(){
     const [temp, setTemp] = useState(15)
     const [mode, setMode] = useState("Hot")
+    const [units, setUnits] = useState("F")
+
+    function convertUnits(){
+        setTemp(temp+10)
+    }
     return(
     
         <div>
-            <h1>{temp}</h1>
+            <h2>{mode}</h2>
+
+            <h1>{temp} &nbsp; {units}</h1>
             <button onClick={()=>{setTemp(temp+1)}}>+</button>
             <button onClick={()=>{setTemp(temp-1)}}>-</button>
 
-            <h2>{mode}</h2>
+            
             <hr />
             <br />
 
             <button onClick={()=>{setMode("Auto")}}>Auto</button>
             <button onClick={()=>{setMode("Hot")}}>Hot</button>
             <button onClick={()=>{setMode("Cold")}}>Cold</button>
+
+            <br />
+            <hr />
+            <br />
+
+            <button onClick={()=>{setUnits("F")}}>F</button>
+            <button onClick={()=>{setUnits("C")}}>C</button>
+
+            <button onClick={()=>{convertUnits()}}>Convert</button>
+
 
 
         </div>
