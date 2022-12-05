f9457d8c277cf294ffc57ebcda808cccb0150264
SK627@myscc.ca <SK627@myscc.ca>
2022-12-04 23:47:40 -0500

    changes assignment 4

--- assignments/src/App.js
+++ assignments/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { useState } from "react";
 
 function App() {
+  const [status, setStatus] = useState(false);
+
+  const authenticate = ()=>{
+    setStatus(true)
+  }
+
+  const logout = ()=>{
+    setStatus(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Login auth={authenticate}/>} />
+        <Route path="/home" element={<Home logout={logout} status={status}/>} />
+      </Routes>
     </div>
   );
 }
